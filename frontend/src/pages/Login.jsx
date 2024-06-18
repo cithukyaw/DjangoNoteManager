67e6544bf7b39229b9b8f6b08bb33fba41589110
Sithu <cithukyaw@gmail.com>
2024-06-18 15:49:18 +0630

    [frontend] Add connected link between login and register

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,12 @@
 import Form from "../components/Form.jsx";
 
 function Login() {
-    return <Form route="/api/token" method="login" />
+    return (
+        <>
+            <Form route="/api/token" method="login" />
+            <a href='/register'>Create an account</a>
+        </>
+    );
 }
 
 export default Login;
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,7 +1,12 @@
 import Form from "../components/Form.jsx";
 
 function Register() {
-    return <Form route="/api/user/register" method="register" />
+    return (
+        <>
+            <Form route="/api/user/register" method="register"/>
+            <a href='/login'>If you already have an account, login here.</a>
+        </>
+    );
 }
 
 export default Register;
