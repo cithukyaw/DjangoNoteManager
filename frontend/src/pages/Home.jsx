f5bd330b318e7726e1ab0fd40e790310d92e9d77
Sithu <cithukyaw@gmail.com>
2024-06-18 15:24:44 +0630

    [frontend] Update style

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -65,26 +65,28 @@ function Home() {
             </div>
             <h2>Create a Note</h2>
             <form onSubmit={createNote}>
-                <label htmlFor="title">Title:</label>
-                <br />
-                <input
-                    type="text"
-                    id="title"
-                    name="title"
-                    required
-                    onChange={(e) => setTitle(e.target.value)}
-                    value={title}
-                />
-                <label htmlFor="content">Content:</label>
-                <br />
-                <textarea
-                    id="content"
-                    name="content"
-                    required
-                    value={content}
-                    onChange={(e) => setContent(e.target.value)}
-                ></textarea>
-                <br />
+                <div>
+                    <label htmlFor="title">Title:</label>
+                    <input
+                        type="text"
+                        id="title"
+                        name="title"
+                        required
+                        onChange={(e) => setTitle(e.target.value)}
+                        value={title}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="content">Content:</label>
+                    <textarea
+                        id="content"
+                        name="content"
+                        required
+                        value={content}
+                        rows={5}
+                        onChange={(e) => setContent(e.target.value)}
+                    ></textarea>
+                </div>
                 {loading && <LoadingIndicator />}
                 <input type="submit" value="Submit"></input>
             </form>
