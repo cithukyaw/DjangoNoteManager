d26e6b242db678af9f0c7ddc8f70ed4edb74df10
Sithu <cithukyaw@gmail.com>
2024-06-18 20:20:04 +0630

    [frontend] Add name field to register form

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -8,20 +8,26 @@ import {FormError} from "./FormError.jsx";
 
 // eslint-disable-next-line react/prop-types
 function Form({ route, method }) {
+    const [name, setName] = useState("");
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
     const [loading, setLoading] = useState(false);
     const [errors, setErrors] = useState({});
     const navigate = useNavigate();
 
-    const name = method === 'login' ? 'Login' : 'Register';
+    const title = method === 'login' ? 'Login' : 'Register';
 
     const handleSubmit = async (e) => {
         e.preventDefault();
         setLoading(true);
 
         try {
-            const res = await api.post(route, {username, password})
+            const res = await api.post(route, {
+                first_name: name,
+                username,
+                password
+            });
+
             if (method === 'login') {
                 localStorage.setItem(ACCESS_TOKEN, res.data.access);
                 localStorage.setItem(REFRESH_TOKEN, res.data.refresh);
@@ -42,9 +48,23 @@ function Form({ route, method }) {
     }
 
     return <form onSubmit={handleSubmit} className="form-container">
-        <h1>{name}</h1>
+        <h1>{title}</h1>
         <div>
             <FormError errors={errors} field="detail"></FormError>
+        </div>
+        { method === 'register' &&
+            <div>
+                <input
+                    className={'form-input' + (errors.hasOwnProperty('name') ? ' input-error ' : '')}
+                    type="text"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    placeholder="Full Name"
+                />
+                <FormError errors={errors} field="name"></FormError>
+            </div>
+        }
+        <div>
             <input
                 className={'form-input' + (errors.hasOwnProperty('username') ? ' input-error ' : '')}
                 type="text"
@@ -65,7 +85,7 @@ function Form({ route, method }) {
             <FormError errors={errors} field="password"></FormError>
         </div>
         {loading && <LoadingIndicator/>}
-        <button className="form-button" type="submit">{name}</button>
+        <button className="form-button" type="submit">{title}</button>
     </form>
 }
 
